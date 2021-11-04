486b37726513956cdcd09cef9daf69793e48ca63
Long Nguyen <ngoclong0x@gmail.com>
2021-11-04 16:40:14 +0800

    Change the columns of database

--- server_database/app.py
+++ server_database/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'YOUR_DB_URL'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qffyrbbcfnypkx:a4ded0fbf801a7dda4a952f078a1bbb34f0735bcabae112ac608632f63980d83@ec2-35-153-88-219.compute-1.amazonaws.com:5432/d9e07vb27udefh'
 	
 db = SQLAlchemy(app)
 
@@ -14,37 +14,39 @@ class User(db.Model):
 
 	# Makes three columns into the table id, name, email
 	_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	name = db.Column(db.String(100), nullable=False)
-	email = db.Column(db.String(100), nullable=False)
+	timecollect = db.Column(db.Integer)
+	acc_x_neck = db.Column(db.Float(), nullable=False)
+	acc_y_neck = db.Column(db.Float(), nullable=False)
+	acc_z_neck = db.Column(db.Float(), nullable=False)
 
 	# A constructor function where we will pass the name and email of a user and it gets add as a new entry in the table.
-	def __init__(self, name, email):
-		self.name = name
-		self.email = email
+	def __init__(self, timecollect, acc_x_neck, acc_y_neck, acc_z_neck):
+		self.timecollect = timecollect
+		self.acc_x_neck = acc_x_neck
+		self.acc_y_neck = acc_y_neck
+		self.acc_z_neck = acc_z_neck
 
 # Control will come here and then gets redirect to the index function
 @app.route("/")
 def home():
-	return redirect(url_for('index'))
+	return render_template("demo3237.html", user_data = User.query.all())
 
-
-@app.route("/index", methods = ["GET", "POST"])
-def index():
-	if request.method == 'POST': # When a user clicks submit button it will come here.
-		data = request.form # request the data from the form in index.html file
-		name = data["name"]
-		email = data["email"]
-
-		new_data = User(name, email)
+@app.route('/add_data', methods = ["GET", "POST", "PUT"]) 
+def add_data():
+	if request.method == 'PUT': # When a user clicks submit button it will come here.
+		#data = request.args.get # request the data from the form in index.html file
+		timecollect = int(request.args.get("timecollect"))
+		acc_x_neck = float(request.args.get("acc_x_neck"))
+		acc_y_neck = float(request.args.get("acc_y_neck"))
+		acc_z_neck = float(request.args.get("acc_z_neck"))
+		new_data = User(timecollect, acc_x_neck, acc_y_neck, acc_z_neck)
 		db.session.add(new_data)
 		db.session.commit()
 
 		user_data = User.query.all()
+		return render_template("demo3237.html", user_data = user_data) # passes user_data variable into the index.html file.
 
-		return render_template("index.html", user_data = user_data) # passes user_data variable into the index.html file.
-
-	return render_template("index.html")
-	
+	return render_template("demo3237.html", user_data = User.query.all())
 
 if __name__=="__main__":
 	db.create_all()
