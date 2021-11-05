9ad0e28d5ae4999632c5502a780c4e0ef27995be
Looi Kai Wen <e0406836@u.nus.edu>
2021-11-05 17:50:18 +0800

    Added delete func

--- server_database/app.py
+++ server_database/app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///User.sqlite3'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///User.sqlite3'
 	
 db = SQLAlchemy(app)
 class User(db.Model):
@@ -13,6 +14,7 @@ class User(db.Model):
 
 	# Makes three columns into the table id, name, email
 	_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String(100))
 	timecollect = db.Column(db.Integer)
 	acc_x_neck = db.Column(db.Float(), nullable=False)
 	acc_y_neck = db.Column(db.Float(), nullable=False)
@@ -21,12 +23,14 @@ class User(db.Model):
 
 
 	# A constructor function where we will pass the name and email of a user and it gets add as a new entry in the table.
-	def __init__(self, timecollect, acc_x_neck, acc_y_neck, acc_z_neck, classification):
+	def __init__(self, name, timecollect, acc_x_neck, acc_y_neck, acc_z_neck, classification):
+		self.name = name
 		self.timecollect = timecollect
 		self.acc_x_neck = acc_x_neck
 		self.acc_y_neck = acc_y_neck
 		self.acc_z_neck = acc_z_neck
 		self.classification = classification
+		
 
 # Control will come here and then gets redirect to the index function
 @app.route("/")
@@ -37,12 +41,13 @@ def home():
 def add_data():
 	if request.method == 'PUT': # When a user clicks submit button it will come here.
 		#data = request.args.get # request the data from the form in index.html file
+		name = request.args.get("name")
 		timecollect = int(request.args.get("timecollect"))
 		acc_x_neck = float(request.args.get("acc_x_neck"))
 		acc_y_neck = float(request.args.get("acc_y_neck"))
 		acc_z_neck = float(request.args.get("acc_z_neck"))
 		classification = float(request.args.get("classification"))
-		new_data = User(timecollect, acc_x_neck, acc_y_neck, acc_z_neck, classification)
+		new_data = User(name, timecollect, acc_x_neck, acc_y_neck, acc_z_neck, classification)
 		db.session.add(new_data)
 		db.session.commit()
 
@@ -62,6 +67,17 @@ def get_data():
 
 		result = {'Straight': 20, 'Lean forward': 20, 'Lean backward': 20, 'Lean right': 20, 'Lean left': 20}
 	return result, 200
+
+@app.route('/remove_data_all', methods = ["POST"]) 
+def remove_data_all():
+	if request.method == 'POST': # When a user clicks submit button it will come here.
+		name = request.args.get("name")
+		entries_to_delete = User.query.filter_by(name=name).all()
+		for entry in entries_to_delete:
+			db.session.delete(entry)
+			db.session.commit()
+
+	return "None", 200
 
 if __name__=="__main__":
 	db.create_all()
