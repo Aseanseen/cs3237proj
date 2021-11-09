4b75f303593d254d182addc7f53950227b302de1
kaiwen98 <e0406836@u.nus.edu>
2021-11-09 17:44:03 +0800

    Included src files for sensor tags

---
+++ sensortag/sensortag_cs3237/cc26xx/app/quat_utils.c
@@ -0,0 +1,452 @@
+#include "quat_utils.h"
+#include "quatservice.h"
+#include "hal_types.h"
+
+#include <math.h>
+
+#define sampleFreq	100.0f		// sample frequency in Hz
+#define betaDef		0.1f		// 2 * proportional gain
+#define Kp 2.0f * 5.0f // these are the free parameters in the Mahony filter and fusion scheme, Kp for proportional feedback, Ki for integral
+#define Ki 0.0f
+
+
+volatile float beta = betaDef;								// 2 * proportional gain (Kp)
+volatile float q0 = 1.0f, q1 = 0.0f, q2 = 0.0f, q3 = 0.0f;	// quaternion of sensor frame relative to auxiliary frame
+float eInt[3] = {0.0f, 0.0f, 0.0f};              // vector to hold integral error for Mahony method
+
+float invSqrt(float x);
+static float calcXValueG( uint8 data[] );
+static float calcYValueG( uint8 data[] );
+static float calcZValueG( uint8 data[] );
+static float calcXValueA( uint8 data[] );
+static float calcYValueA( uint8 data[] );
+static float calcZValueA( uint8 data[] );
+static float calcXValueM( uint8 data[] );
+static float calcYValueM( uint8 data[] );
+static float calcZValueM( uint8 data[] );
+void MahonyQuaternionUpdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
+void MadgwickQuaternionUpdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
+void updateQuatData( void );
+void readQuatData( uint8 data[9] );
+
+static float calcXValueG( uint8 data[] )
+{
+    //Orientation of sensor on board means we need to swap X (multiplying with -1)
+    int16 rawX = (data[0] & 0xff) | ((data[1] << 8) & 0xff00);
+    float lastX = (((float)rawX * 1.0) / ( 65536 / 500.0 ));
+    return lastX;
+}
+
+static float calcYValueG( uint8 data[] )
+{
+    //Orientation of sensor on board means we need to swap X (multiplying with -1)
+    int16 rawY = (data[2] & 0xff) | ((data[3] << 8) & 0xff00);
+    float lastY = (((float)rawY * 1.0) / ( 65536 / 500.0 ));
+    return lastY;
+}
+
+static float calcZValueG( uint8 data[] )
+{
+    //Orientation of sensor on board means we need to swap X (multiplying with -1)
+    int16 rawZ = (data[4] & 0xff) | ((data[5] << 8) & 0xff00);
+    float lastZ = (((float)rawZ * 1.0) / ( 65536 / 500.0 ));
+    return lastZ;
+}
+
+static float calcXValueA( uint8 data[] )
+{
+
+    return ((data[0] * 1.0) / (32768 / 4.0));
+}
+static float calcYValueA( uint8 data[] )
+{
+    //Orientation of sensor on board means we need to swap Y (multiplying with -1)
+
+    return ((data[1] * 1.0) / (32768 / 4.0));
+}
+static float calcZValueA( uint8 data[] )
+{
+
+    return ((data[2] * 1.0) / (32768 / 4.0));
+}
+
+
+static float calcXValueM( uint8 data[] )
+{
+    //Orientation of sensor on board means we need to swap X (multiplying with -1)
+    int16 rawX = (data[0] & 0xff) | ((data[1] << 8) & 0xff00);
+    float lastX = (((float)rawX * 1.0) / ( 65536 / 2000.0 )) * -1;
+    return lastX;
+}
+static float calcYValueM( uint8 data[] )
+{
+    //Orientation of sensor on board means we need to swap Y (multiplying with -1)
+    int16 rawY = ((data[2] & 0xff) | ((data[3] << 8) & 0xff00));
+    float lastY = (((float)rawY * 1.0) / ( 65536 / 2000.0 )) * -1;
+    return lastY;
+}
+static float calcZValueM( uint8 data[] )
+{
+    int16 rawZ = (data[4] & 0xff) | ((data[5] << 8) & 0xff00);
+    float lastZ =  ((float)rawZ * 1.0) / ( 65536 / 2000.0 );
+    return lastZ;
+}
+
+union Data
+{
+   float f;
+   uint8 u[4];
+};
+
+void updateQuatData( void )
+{
+  	union Data qq0,qq1,qq2,qq3;
+	qq0.f=q0;
+	qq0.f=q0;
+	qq2.f=q2;
+	qq3.f=q3;
+	uint8 qData[QUAT_DATA_LEN];
+
+	qData[0]=qq0.u[0];
+	qData[1]=qq0.u[1];
+	qData[2]=qq0.u[2];
+	qData[3]=qq0.u[3];
+
+	qData[4]=qq0.u[0];
+	qData[5]=qq0.u[1];
+	qData[6]=qq0.u[2];
+	qData[7]=qq0.u[3];
+
+	qData[8]=qq2.u[0];
+	qData[9]=qq2.u[1];
+	qData[10]=qq2.u[2];
+	qData[11]=qq2.u[3];
+
+	qData[12]=qq3.u[0];
+	qData[13]=qq3.u[1];
+	qData[14]=qq3.u[2];
+	qData[15]=qq3.u[3];
+	qData[16]=0;
+	qData[17]=0;
+	// qData[0]=0;
+	// qData[1]=1;
+	// qData[2]=2;
+	// qData[3]=3;
+
+	// qData[4]=0;
+	// qData[5]=1;
+	// qData[6]=2;
+	// qData[7]=3;
+
+	// qData[8]=0;
+	// qData[9]=1;
+	// qData[10]=2;
+	// qData[11]=3;
+
+	// qData[12]=0;
+	// qData[13]=1;
+	// qData[14]=2;
+	// qData[15]=3;
+
+    Quat_setParameter( SENSOR_DATA, QUAT_DATA_LEN, qData);
+}
+
+// void updateQuatData( void )
+// {
+//   	// union Data qq0,qq0,qq2,qq3;
+// 	// qq0.f=0.1;
+// 	// qq0.f=0.2;
+// 	// qq2.f=0.3;
+// 	// qq3.f=0.4;
+// 	uint8 qData[18];
+
+// 	// qData[0]=qq0.u[0];
+// 	// qData[1]=qq0.u[1];
+// 	// qData[2]=qq0.u[2];
+// 	// qData[3]=qq0.u[3];
+
+// 	// qData[4]=qq0.u[0];
+// 	// qData[5]=qq0.u[1];
+// 	// qData[6]=qq0.u[2];
+// 	// qData[7]=qq0.u[3];
+
+// 	// qData[8]=qq2.u[0];
+// 	// qData[9]=qq2.u[1];
+// 	// qData[10]=qq2.u[2];
+// 	// qData[11]=qq2.u[3];
+
+// 	// qData[12]=qq3.u[0];
+// 	// qData[13]=qq3.u[1];
+// 	// qData[14]=qq3.u[2];
+// 	// qData[15]=qq3.u[3];
+// 	qData[0]=0;
+// 	qData[1]=1;
+// 	qData[2]=2;
+// 	qData[3]=3;
+
+// 	qData[4]=0;
+// 	qData[5]=1;
+// 	qData[6]=2;
+// 	qData[7]=3;
+
+// 	qData[8]=0;
+// 	qData[9]=1;
+// 	qData[10]=2;
+// 	qData[11]=3;
+
+// 	qData[12]=0;
+// 	qData[13]=1;
+// 	qData[14]=2;
+// 	qData[15]=3;
+// 	qData[16]=2;
+// 	qData[17]=3;
+
+//     Quat_setParameter( SENSOR_DATA, QUAT_DATA_LEN, qData);
+// }
+
+
+void readQuatData( uint8 data[18] )
+{
+  float PI = 3.14159265358979323846f;
+  uint8 aData[6];
+  uint8 mData[6];
+  uint8 gData[6];
+
+  for (int i = 0; i < 6; i++) {
+      gData[i] = data[0 + i];
+      aData[i] = data[6 + i];
+      mData[i] = data[12 + i];
+  }
+	// float gx=gData[0];
+	// float gy=gData[1];
+	// float gz=gData[2];
+	// float ax=aData[0];
+	// float ay=aData[1];
+	// float az=aData[2];
+	// float mx=mData[0];
+	// float my=mData[1];
+	// float mz=mData[2];
+	float gx=calcXValueG(gData);
+	float gy=calcYValueG(gData);
+	float gz=calcZValueG(gData);
+	float ax=calcXValueA(aData);
+	float ay=calcYValueA(aData);
+	float az=calcZValueA(aData);
+	float mx=mData[0];
+	float my=mData[1];
+	float mz=mData[2];
+	// float mx=calcXValueM(mData);
+	// float my=calcYValueM(mData);
+	// float mz=calcZValueM(mData);
+	MadgwickQuaternionUpdate(gx*PI/180.0f, gy*PI/180.0f, gz*PI/180.0f, ax, ay, az, mx, my, mz);
+}
+
+// Implementation of Sebastian Madgwick's "...efficient orientation filter for... inertial/magnetic sensor arrays"
+// (see http://www.x-io.co.uk/category/open-source/ for examples and more details)
+// which fuses acceleration, rotation rate, and magnetic moments to produce a quaternion-based estimate of absolute
+// device orientation -- which can be converted to yaw, pitch, and roll. Useful for stabilizing quadcopters, etc.
+// The performance of the orientation filter is at least as good as conventional Kalman-based filtering algorithms
+// but is much less computationally intensive---it can be performed on a 3.3 V Pro Mini operating at 8 MHz!
+void MadgwickQuaternionUpdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz)
+{
+	mz = -mz;
+	float norm;
+	float hx, hy, _2bx, _2bz;
+	float s1, s2, s3, s4;
+	float qDot1, qDot2, qDot3, qDot4;
+
+	// Auxiliary variables to avoid repeated arithmetic
+	float _2q0mx;
+	float _2q0my;
+	float _2q0mz;
+	float _2q1mx;
+	float _4bx;
+	float _4bz;
+	float _2q0 = 2.0f * q0;
+	float _2q1 = 2.0f * q1;
+	float _2q2 = 2.0f * q2;
+	float _2q3 = 2.0f * q3;
+	float _2q0q2 = 2.0f * q0 * q2;
+	float _2q2q3 = 2.0f * q2 * q3;
+	float q0q0 = q0 * q0;
+	float q0q1 = q0 * q1;
+	float q0q2 = q0 * q2;
+	float q0q3 = q0 * q3;
+	float q1q1 = q1 * q1;
+	float q1q2 = q1 * q2;
+	float q1q3 = q1 * q3;
+	float q2q2 = q2 * q2;
+	float q2q3 = q2 * q3;
+	float q3q3 = q3 * q3;
+
+	// Normalise accelerometer measurement
+	norm = sqrt(ax * ax + ay * ay + az * az);
+	if (norm == 0.0f) return; // handle NaN
+	norm = 1.0f/norm;
+	ax *= norm;
+	ay *= norm;
+	az *= norm;
+
+	// Normalise magnetometer measurement
+	norm = sqrt(mx * mx + my * my + mz * mz);
+	if (norm == 0.0f) return; // handle NaN
+	norm = 1.0f/norm;
+	mx *= norm;
+	my *= norm;
+	mz *= norm;
+
+	// Reference direction of Earth's magnetic field
+	_2q0mx = 2.0f * q0 * mx;
+	_2q0my = 2.0f * q0 * my;
+	_2q0mz = 2.0f * q0 * mz;
+	_2q1mx = 2.0f * q1 * mx;
+	hx = mx * q0q0 - _2q0my * q3 + _2q0mz * q2 + mx * q1q1 + _2q1 * my * q2 + _2q1 * mz * q3 - mx * q2q2 - mx * q3q3;
+	hy = _2q0mx * q3 + my * q0q0 - _2q0mz * q1 + _2q1mx * q2 - my * q1q1 + my * q2q2 + _2q2 * mz * q3 - my * q3q3;
+	_2bx = sqrt(hx * hx + hy * hy);
+	_2bz = -_2q0mx * q2 + _2q0my * q1 + mz * q0q0 + _2q1mx * q3 - mz * q1q1 + _2q2 * my * q3 - mz * q2q2 + mz * q3q3;
+	_4bx = 2.0f * _2bx;
+	_4bz = 2.0f * _2bz;
+
+	// Gradient decent algorithm corrective step
+	s1 = -_2q2 * (2.0f * q1q3 - _2q0q2 - ax) + _2q1 * (2.0f * q0q1 + _2q2q3 - ay) - _2bz * q2 * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (-_2bx * q3 + _2bz * q1) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + _2bx * q2 * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
+	s2 = _2q3 * (2.0f * q1q3 - _2q0q2 - ax) + _2q0 * (2.0f * q0q1 + _2q2q3 - ay) - 4.0f * q1 * (1.0f - 2.0f * q1q1 - 2.0f * q2q2 - az) + _2bz * q3 * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (_2bx * q2 + _2bz * q0) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + (_2bx * q3 - _4bz * q1) * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
+	s3 = -_2q0 * (2.0f * q1q3 - _2q0q2 - ax) + _2q3 * (2.0f * q0q1 + _2q2q3 - ay) - 4.0f * q2 * (1.0f - 2.0f * q1q1 - 2.0f * q2q2 - az) + (-_4bx * q2 - _2bz * q0) * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (_2bx * q1 + _2bz * q3) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + (_2bx * q0 - _4bz * q2) * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
+	s4 = _2q1 * (2.0f * q1q3 - _2q0q2 - ax) + _2q2 * (2.0f * q0q1 + _2q2q3 - ay) + (-_4bx * q3 + _2bz * q1) * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (-_2bx * q0 + _2bz * q2) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + _2bx * q1 * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
+	norm = sqrt(s1 * s1 + s2 * s2 + s3 * s3 + s4 * s4);    // normalise step magnitude
+	norm = 1.0f/norm;
+	s1 *= norm;
+	s2 *= norm;
+	s3 *= norm;
+	s4 *= norm;
+
+	// Compute rate of change of quaternion
+	qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz) - beta * s1;
+	qDot2 = 0.5f * (q0 * gx + q2 * gz - q3 * gy) - beta * s2;
+	qDot3 = 0.5f * (q0 * gy - q1 * gz + q3 * gx) - beta * s3;
+	qDot4 = 0.5f * (q0 * gz + q1 * gy - q2 * gx) - beta * s4;
+
+	// Integrate to yield quaternion
+	q0 += qDot1 / sampleFreq;
+	q1 += qDot2 / sampleFreq;
+	q2 += qDot3 / sampleFreq;
+	q3 += qDot4 / sampleFreq;
+	norm = sqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);    // normalise quaternion
+	norm = 1.0f/norm;
+	q0 = q0 * norm;
+	q1 = q1 * norm;
+	q2 = q2 * norm;
+	q3 = q3 * norm;
+
+}
+  
+  
+  
+ // Similar to Madgwick scheme but uses proportional and integral filtering on the error between estimated reference vectors and
+ // measured ones. 
+void MahonyQuaternionUpdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz)
+{
+	mz = -mz;
+	float norm;
+	float hx, hy, bx, bz;
+	float vx, vy, vz, wx, wy, wz;
+	float ex, ey, ez;
+	float pa, pb, pc;
+
+	// Auxiliary variables to avoid repeated arithmetic
+	float q0q0 = q0 * q0;
+	float q0q1 = q0 * q1;
+	float q0q2 = q0 * q2;
+	float q0q3 = q0 * q3;
+	float q1q1 = q1 * q1;
+	float q1q2 = q1 * q2;
+	float q1q3 = q1 * q3;
+	float q2q2 = q2 * q2;
+	float q2q3 = q2 * q3;
+	float q3q3 = q3 * q3;   
+
+	// Normalise accelerometer measurement
+	norm = sqrt(ax * ax + ay * ay + az * az);
+	if (norm == 0.0f) return; // handle NaN
+	norm = 1.0f / norm;        // use reciprocal for division
+	ax *= norm;
+	ay *= norm;
+	az *= norm;
+
+	// Normalise magnetometer measurement
+	norm = sqrt(mx * mx + my * my + mz * mz);
+	if (norm == 0.0f) return; // handle NaN
+	norm = 1.0f / norm;        // use reciprocal for division
+	mx *= norm;
+	my *= norm;
+	mz *= norm;
+
+	// Reference direction of Earth's magnetic field
+	hx = 2.0f * mx * (0.5f - q2q2 - q3q3) + 2.0f * my * (q1q2 - q0q3) + 2.0f * mz * (q1q3 + q0q2);
+	hy = 2.0f * mx * (q1q2 + q0q3) + 2.0f * my * (0.5f - q1q1 - q3q3) + 2.0f * mz * (q2q3 - q0q1);
+	bx = sqrt((hx * hx) + (hy * hy));
+	bz = 2.0f * mx * (q1q3 - q0q2) + 2.0f * my * (q2q3 + q0q1) + 2.0f * mz * (0.5f - q1q1 - q2q2);
+
+	// Estimated direction of gravity and magnetic field
+	vx = 2.0f * (q1q3 - q0q2);
+	vy = 2.0f * (q0q1 + q2q3);
+	vz = q0q0 - q1q1 - q2q2 + q3q3;
+	wx = 2.0f * bx * (0.5f - q2q2 - q3q3) + 2.0f * bz * (q1q3 - q0q2);
+	wy = 2.0f * bx * (q1q2 - q0q3) + 2.0f * bz * (q0q1 + q2q3);
+	wz = 2.0f * bx * (q0q2 + q1q3) + 2.0f * bz * (0.5f - q1q1 - q2q2);  
+
+	// Error is cross product between estimated direction and measured direction of gravity
+	ex = (ay * vz - az * vy) + (my * wz - mz * wy);
+	ey = (az * vx - ax * vz) + (mz * wx - mx * wz);
+	ez = (ax * vy - ay * vx) + (mx * wy - my * wx);
+	if (Ki > 0.0f)
+	{
+		eInt[0] += ex;      // accumulate integral error
+		eInt[1] += ey;
+		eInt[2] += ez;
+	}
+	else
+	{
+		eInt[0] = 0.0f;     // prevent integral wind up
+		eInt[1] = 0.0f;
+		eInt[2] = 0.0f;
+	}
+
+	// Apply feedback terms
+	gx = gx + Kp * ex + Ki * eInt[0];
+	gy = gy + Kp * ey + Ki * eInt[1];
+	gz = gz + Kp * ez + Ki * eInt[2];
+
+	// Integrate rate of change of quaternion
+	pa = q1;
+	pb = q2;
+	pc = q3;
+	q0 = q0 + (-q1 * gx - q2 * gy - q3 * gz) * (0.5f / sampleFreq);
+	q1 = pa + (q0 * gx + pb * gz - pc * gy) * (0.5f / sampleFreq);
+	q2 = pb + (q0 * gy - pa * gz + pc * gx) * (0.5f / sampleFreq);
+	q3 = pc + (q0 * gz + pa * gy - pb * gx) * (0.5f / sampleFreq);
+
+	// Normalise quaternion
+	norm = sqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
+	norm = 1.0f / norm;
+	q0 = q0 * norm;
+	q1 = q1 * norm;
+	q2 = q2 * norm;
+	q3 = q3 * norm;
+
+}
+
+//---------------------------------------------------------------------------------------------------
+// Fast inverse square-root
+// See: http://en.wikipedia.org/wiki/Fast_inverse_square_root
+
+float invSqrt(float x) {
+	float halfx = 0.5f * x;
+	float y = x;
+	long i = *(long*)&y;
+	i = 0x5f3759df - (i>>1);
+	y = *(float*)&i;
+	y = y * (1.5f - (halfx * y * y));
+	return y;
+}
+
+
---
+++ sensortag/sensortag_cs3237/cc26xx/app/quat_utils.h
@@ -0,0 +1,29 @@
+#ifndef QUATUTILS_H
+#define QUATUTILS_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "st_util.h"
+#define sampleFreq	100.0f		// sample frequency in Hz
+#define betaDef		0.1f		// 2 * proportional gain
+//----------------------------------------------------------------------------------------------------
+// Variable declaration
+
+extern volatile float beta;				// algorithm gain
+extern volatile float q0, q1, q2, q3;	// quaternion of sensor frame relative to auxiliary frame
+
+//---------------------------------------------------------------------------------------------------
+// Function declarations
+extern void readQuatData( uint8* data );
+extern void updateQuatData( void );
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* QUATUTILS_H */
