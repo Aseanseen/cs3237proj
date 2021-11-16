b582cccb4ccd25ff42dd91557eea6298353d7391
Long Nguyen <ngoclong0x@gmail.com>
2021-11-16 18:27:44 +0800

    Update classify from MQTT broker

--- mqtt_broker/model.py
+++ mqtt_broker/model.py
@@ -5,6 +5,7 @@ from commons import (
 )
 import numpy as np
 import json
+import tensorflow as tf
 
 def string_to_float_list(string_of_list):
     list_of_strings = string_of_list.strip('][').split(', ')
@@ -18,21 +19,23 @@ def classify(recv_dict):
     input_array = recv_dict
     
     model_input = np.array(input_array).reshape(1, -1)
-    print(model)
+
     try:
         result = model.predict(model_input)[0]
     except:
         result = -1
     
+    max_index = np.argmax(result)
+
     #Create an answer object
     answer_dict = {}
-    answer_dict['classification'] = label_dict[result]
+    answer_dict['classification'] = label_dict[max_index]
     
     return answer_dict
 
 def load_model():
     global model 
-    filename = 'rfc_model.sav'
-    model = joblib.load(filename)
+    filename = 'model_nn_5'
+    model = tf.keras.models.load_model('model_nn_5')
                                  
 
