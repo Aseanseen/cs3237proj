49e24a501ee6763bd8ed9e6f8e0eb642ed159ab9
Looi Kai Wen <e0406836@u.nus.edu>
2021-11-18 18:17:41 +0800

    ypr

--- mqtt_broker/model.py
+++ mqtt_broker/model.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, render_template
 import joblib
 from commons import (
-    CLASSIFICATION_TO_MQTT_CLASSIFICATION
+    CLASSIFICATION_TO_MQTT_CLASSIFICATION,
+    MQTT_CLASSIFICATION_BACKWARD,
+    MQTT_CLASSIFICATION_FORWARD,
+    MQTT_CLASSIFICATION_LEFT,
+    MQTT_CLASSIFICATION_PROPER,
+    MQTT_CLASSIFICATION_RIGHT
 )
 import numpy as np
 import json
 import tensorflow as tf
+
+from utils import (
+    getEulerAngles
+)
 
 def string_to_float_list(string_of_list):
     list_of_strings = string_of_list.strip('][').split(', ')
@@ -15,22 +24,24 @@ def string_to_float_list(string_of_list):
 def classify(recv_dict):
     label_dict = CLASSIFICATION_TO_MQTT_CLASSIFICATION
     label_dict[-1] = "Error has occured, model is not predicting..."
-    
-    input_array = recv_dict
-    
-    model_input = np.array(input_array).reshape(1, -1)
-
-    try:
-        result = model.predict(model_input)[0]
-    except:
-        result = -1
-    
-    max_index = np.argmax(result)
-
-    #Create an answer object
     answer_dict = {}
-    answer_dict['classification'] = label_dict[max_index]
+    postfix = ["_back_mid"]
+    q_ls = ["q0", "q1", "q2", "q3"]
+
+    for pf in postfix:
+        y, p, r = getEulerAngles([recv_dict[i + pf] for i in q_ls])
     
+    if abs(r) < 180 - 30:
+        answer_dict['classification'] = MQTT_CLASSIFICATION_FORWARD
+    elif abs(r) > 180 + 30:
+        answer_dict['classification'] = MQTT_CLASSIFICATION_BACKWARD
+    else:
+        if p < 0 - 17:
+            answer_dict['classification'] = MQTT_CLASSIFICATION_LEFT
+        elif p > 0 + 17: 
+            answer_dict['classification'] = MQTT_CLASSIFICATION_RIGHT
+        else: 
+            answer_dict['classification'] = MQTT_CLASSIFICATION_PROPER
     return answer_dict
 
 def load_model():
